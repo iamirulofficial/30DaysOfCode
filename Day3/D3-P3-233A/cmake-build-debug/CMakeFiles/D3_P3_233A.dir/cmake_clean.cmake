f9918780f3bc623a3a539861a34719d38b206848
Amirul <aadipapsarponmisti@gmail.com>
2021-01-13 12:07:47 +0530

    /D3-P3-233A done 🎆

---
+++ Day3/D3-P3-233A/cmake-build-debug/CMakeFiles/D3_P3_233A.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/D3_P3_233A.dir/main.cpp.obj"
+  "D3_P3_233A.exe"
+  "D3_P3_233A.exe.manifest"
+  "D3_P3_233A.pdb"
+  "libD3_P3_233A.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/D3_P3_233A.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
