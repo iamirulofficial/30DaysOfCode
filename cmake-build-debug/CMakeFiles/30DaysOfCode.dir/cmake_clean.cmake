2cdd1baa22984f05690382aa33fe9930c44fc043
Amirul <aadipapsarponmisti@gmail.com>
2021-01-13 10:50:30 +0530

    /codeforces-link-inside-the-program 🎉

---
+++ cmake-build-debug/CMakeFiles/30DaysOfCode.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "30DaysOfCode.exe"
+  "30DaysOfCode.exe.manifest"
+  "30DaysOfCode.pdb"
+  "CMakeFiles/30DaysOfCode.dir/Day3/problem1.cpp.obj"
+  "lib30DaysOfCode.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/30DaysOfCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
