4c989aa02de935ee2441c8f085e630b12f0e8f2f
Amirul <aadipapsarponmisti@gmail.com>
2021-01-14 23:56:15 +0530

    /2problems 🎄🎄

---
+++ Day4/D2-P5/cmake-build-debug/CMakeFiles/D2_P5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/D2_P5.dir/214a.cpp.obj"
+  "D2_P5.exe"
+  "D2_P5.exe.manifest"
+  "D2_P5.pdb"
+  "libD2_P5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/D2_P5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
