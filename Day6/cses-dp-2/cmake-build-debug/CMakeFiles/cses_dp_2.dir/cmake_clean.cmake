7eae54fd4e0fb83e4f2882d615eef7c9ad174a7a
Amirul <aadipapsarponmisti@gmail.com>
2021-01-22 18:29:08 +0530

    /cses-dp-2 🍕

---
+++ Day6/cses-dp-2/cmake-build-debug/CMakeFiles/cses_dp_2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cses_dp_2.dir/main.cpp.obj"
+  "cses_dp_2.exe"
+  "cses_dp_2.exe.manifest"
+  "cses_dp_2.pdb"
+  "libcses_dp_2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cses_dp_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
