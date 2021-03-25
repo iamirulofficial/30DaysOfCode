2e230e6ed75918e928fa69d30dd58c6cd12712e9
Amirul <aadipapsarponmisti@gmail.com>
2021-03-25 21:39:18 +0530

    💥

---
+++ gfg-2/main.cpp
@@ -0,0 +1,22 @@
+// Ami || Code-chef || HackerEarth || Codeforces ||
+//amirul.cse.1804@iiitbh.ac.in
+
+#include <bits/stdc++.h>
+ using namespace std;
+
+#define ll                    long long int
+#define pos(p)                (p<0)?(-1*p):p
+#define MOD                   1000000007
+#define MAX                   1e9
+#define MIN                   -1e9
+#define INF                   (int)1e15
+#define Tests()               int T; scanf("%d",&T); while(T--)
+
+
+signed main()
+{
+	tests(){
+	    
+	}
+	return 0;
+}
